dcdee75932155b04dbf8fce954c0745ba92d37b0
Aitor-RL <aitorrodriguezlomba@gmail.com>
2022-04-04 11:46:42 +0200

    first commit1111

--- src/main/java/com/example/demo/controllers/Ejercicio.java
+++ src/main/java/com/example/demo/controllers/Ejercicio.java
@@ -1,5 +1,7 @@
 package com.example.demo.controllers;
 
+import com.example.demo.utils.Utils;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
--- src/main/java/com/example/demo/utils/Utils.java
+++ src/main/java/com/example/demo/utils/Utils.java
@@ -1,15 +1,10 @@
 package com.example.demo.utils;
 
 public class Utils {
-
-    public static boolean isPalindrome(String value) {
-
+    public static boolean isPalindrome(String value){
         StringBuilder builder = new StringBuilder(value);
-        String reverseName = builder.reverse().toString();
-        return value.equals(reverseName);
-
-        // if(name.equals(reverseName)) return "Si es un palíndromo";
-        // else return "No es un palíndromo";
-
+        String reversedName = builder.reverse().toString();
+        
+        return value.equals(reversedName);
     }
 }
